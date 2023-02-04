bca4b64166ec0a07c83e4f22a43a0403c6145d11
xing <1725256426@qq.com>
2023-02-04 15:34:52 +0800

    fix: base_url

--- src/service/request/conf.ts
+++ src/service/request/conf.ts
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'development') {
   BASE_URL = 'http://152.136.185.210:5000'
   TIME_OUT = 1000
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'https://152.136.185.210:5000'
+  BASE_URL = 'http://152.136.185.210:5000'
 } else if (process.env.NODE_ENV === 'test') {
   BASE_URL = 'httpbin.org/ '
 }
